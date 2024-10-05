d6dfc96679c9ab01adb84cc19d0c1853cda75e4b
thomas <psychoid@hotmail.fr>
2024-10-05 16:14:26 +0200

    model amount cree presque finit

--- omega_back/budget/models.py
+++ omega_back/budget/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Amount(models.Model):
+
+    name = models.CharField(max_length=30, unique=True)
+    active = models.BooleanField(default=True)
+    price = models.IntegerField()
+    cash = models.BooleanField(default=False)
+    automatic_saved = models.BooleanField(default=False)
+    created_date = models.DateField(auto_created=True)
+    modified_date = models.DateField()
+    start_month = models.DateField()
+    each_year = models.SmallIntegerField(default=0)
+    each_month = models.SmallIntegerField(default=0)
+    due = price = models.SmallIntegerField(default=0)
+    alert = models.BooleanField(default=False)
