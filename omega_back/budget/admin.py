454b53b51f008fe99b3ee6e1c1257d914ca4e281
thomas <psychoid@hotmail.fr>
2024-10-08 06:21:47 +0200

    creation des podels en base

--- omega_back/budget/admin.py
+++ omega_back/budget/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Amount, models.Product, models.Shop, models.Price)
+admin.site.register(models.Amount)
+admin.site.register(models.Product)
+admin.site.register(models.Shop)
+admin.site.register(models.Price)
