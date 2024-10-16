db3da55a3d27ab26216028a0e1b28fe55e2da9a1
thomas <psychoid@hotmail.fr>
2024-10-16 05:58:42 +0200

    import de home dans bappiment

--- omega_back/account/admin.py
+++ omega_back/account/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser, Status, Membership
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+
+class CustomUserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('email', 'first_name', 'last_name', 'is_staff')
+    fieldsets = (
+        ('Logs', {'fields': ('email', 'password')}),
+        ('User', {'fields': (
+                    'first_name', 'last_name', 'phone', 'days_off', 
+                    'days_off_cumul', 'hightest_level', 'permanent_contract'
+                    )
+                }
+        ),
+        ('Permission', {'fields': ('is_staff', 'is_active', 'groups')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active'),
+        })
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('first_name', 'last_name')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Membership)
