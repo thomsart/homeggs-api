85a7501f1663c5c89042348eec2ed2b36ccadbe9
thomas <psychoid@hotmail.fr>
2024-10-26 13:46:36 +0200

    reamenagement architecture budget

--- omega_back/budget/urls.py
+++ omega_back/budget/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import company_views, salary_views
+from .views import company, salary
 
 budget_urlpatterns = [
-    path('companies/', company_views.CompanyList.as_view(), name='company-list'),
-    path('companies/<int:pk>/', company_views.CompanyDetail.as_view(), name='company-detail'),
-    path('salaries/', salary_views.SalaryList.as_view(), name='salary-list'),
-    path('companies/<int:pk>/', salary_views.SalaryDetail.as_view(), name='salary-detail'),
+    path('companies/', company.CompanyList.as_view(), name='company-list'),
+    path('companies/<int:pk>/', company.CompanyDetail.as_view(), name='company-detail'),
+    path('salaries/', salary.SalaryList.as_view(), name='salary-list'),
+    path('companies/<int:pk>/', salary.SalaryDetail.as_view(), name='salary-detail'),
 ]
 
 budget_urlpatterns = format_suffix_patterns(budget_urlpatterns)
